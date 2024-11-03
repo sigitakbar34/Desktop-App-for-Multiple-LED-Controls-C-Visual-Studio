698b20118ea8e4490220e954a50753772fc51f96
Sigit Akbar Saputra <89894719+sigitakbar34@users.noreply.github.com>
2024-11-03 23:28:35 +0700

    Add files via upload

---
+++ ESP32 Program/KontrolLED.ino
@@ -0,0 +1,76 @@
+// Definisikan pin LED
+#define L1 18
+#define L2 19
+#define L3 21
+#define L4 22
+#define L5 23
+
+String dataString;
+char data;
+
+void setup() {
+  // Inisialisasi komunikasi serial
+  Serial.begin(115200);
+
+  // Atur semua pin sebagai output
+  pinMode(L1, OUTPUT);
+  pinMode(L2, OUTPUT);
+  pinMode(L3, OUTPUT);
+  pinMode(L4, OUTPUT);
+  pinMode(L5, OUTPUT);
+
+  // Pastikan semua LED dalam keadaan mati di awal
+  digitalWrite(L1, LOW);
+  digitalWrite(L2, LOW);
+  digitalWrite(L3, LOW);
+  digitalWrite(L4, LOW);
+  digitalWrite(L5, LOW);
+}
+
+void loop() {
+  // Periksa apakah ada data yang tersedia di serial
+  if (Serial.available() > 0) {
+    dataString = Serial.readString();
+    char data = dataString.charAt(0);
+
+    // Periksa data yang diterima dan kontrol LED sesuai
+    if (data == 'A') {
+      digitalWrite(L1, HIGH); // Nyalakan LED pada pin 18
+    } 
+    else if (data == 'a') {
+      digitalWrite(L1, LOW); // Matikan LED pada pin 18
+    } 
+    else if (data == 'B') {
+      digitalWrite(L2, HIGH); // Nyalakan LED pada pin 19
+    } 
+    else if (data == 'b') {
+      digitalWrite(L2, LOW); // Matikan LED pada pin 19
+    } 
+    else if (data == 'C') {
+      digitalWrite(L3, HIGH); // Nyalakan LED pada pin 21
+    } 
+    else if (data == 'c') {
+      digitalWrite(L3, LOW); // Matikan LED pada pin 21
+    } 
+    else if (data == 'D') {
+      digitalWrite(L4, HIGH); // Nyalakan LED pada pin 22
+    } 
+    else if (data == 'd') {
+      digitalWrite(L4, LOW); // Matikan LED pada pin 22
+    } 
+    else if (data == 'E') {
+      digitalWrite(L5, HIGH); // Nyalakan LED pada pin 23
+    } 
+    else if (data == 'e') {
+      digitalWrite(L5, LOW); // Matikan LED pada pin 23
+    }
+    // matikan semua led jika disconnect
+    else if (data == 'f') {
+      digitalWrite(L1, LOW);
+      digitalWrite(L2, LOW);
+      digitalWrite(L3, LOW);
+      digitalWrite(L4, LOW);
+      digitalWrite(L5, LOW);
+    }
+  }
+}
